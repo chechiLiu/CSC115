04a4f5fa3c8036b2f6239c25bff37c8a77e8a785
Che-Chi (Jack) Liu <chechiliu@outlook.com>
2019-08-26 16:20:58 -0700

    Update Time.java

--- Assignment 0/Time.java
+++ Assignment 0/Time.java
@@ -1,24 +1,27 @@
-//Che-Chi Jack Liu
+//Che-Chi (Jack) Liu
 //V00850558
 
+/*
+Simple example of a Java class.
+*/
 
 public class Time {
 	private int hours;
 	private int minutes;
 	
-	//constructor must be public, cant have return, its name must be the same as the class name
+	//Constructor must be public, can't have return, and its name must be the same as the class name.
 	public Time(int hours, int minutes) {
 		setHours(hours);
 		setMinutes(minutes);
 	}
 	
-	//another constructor without something to pass in, without parameter
+	//Another constructor without something to pass in; without parameter.
 	public Time() {
 		setHours(0);
 		setMinutes(0);
 	}
-
-	// accessors
+	
+	//Accessors
 	public int getHours() {
 		return this.hours;
 	}
@@ -27,49 +30,49 @@ public class Time {
 		return this.minutes;
 	}
 	
-	// modifiers
+	//Modifiers
 	public void setHours(int hours) {
-		if (hours >= 0 && hours < 24) {
+		if(hours >= 0 && hours < 24) {
 			this.hours = hours;
 		}
-		
 	}
 	
 	public void setMinutes(int minutes) {
-		if ( minutes >= 0 && minutes < 60) {
+		if(minutes >= 0 && minutes < 60) {
 			this.minutes = minutes;
 		}
 	}
 	
+	//Other methods
 	public String toString() {
 		return this.hours + ":" + this.minutes;
 	}
 	
-	public boolean equals (Time other) {
+	public boolean equals(Time other) {
 		return this.hours == other.hours && this.minutes == other.minutes;
 	}
 	
-	public void add (Time other) {
+	public void add(Time other) {
 		int sumHours = this.hours + other.hours;
 		int sumMinutes = this.minutes + other.minutes;
 		
 		if (sumMinutes >= 60) {
 			sumHours += 1;
+			sumMinute -= 60;
 		}
+		
 		this.hours = sumHours%24;
 		this.minutes = sumMinutes%60;
 	}
 	
-	
-	
+	//Testing
 	public static void main (String[] args) {
-		Time t1 = new Time(12, 30);
+		Time t1 = new Time(12, 50);
 		System.out.println("Hours is " + t1.getHours());
 		System.out.println("Minutes is " + t1.getMinutes());
 		System.out.println(t1);
 		
 		Time t2 = new Time();
-		
 		System.out.println(t2);
 		
 		Time t3 = new Time(12, 30);
